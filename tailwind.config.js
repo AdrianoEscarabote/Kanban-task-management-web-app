069daffcb58f4e1afa2d9031dcd4a4815b42ac81
AdrianoEscarabote <drian.escarabote@hotmail.com>
2023-04-13 11:42:59 -0300

    feat: project colors in tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,20 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        purple_Dark: "#635FC7",
+        purple_Light: "#A8A4FF",
+        _dark: "#000112",
+        almost_Dark: "#20212C",
+        dark_Gray: "#2B2C37",
+        medium_Gray: "#3E3F4E",
+        _gray: "#828FA3",
+        light_Blue: "#E4EBFA",
+        almost_White: "#F4F7FD",
+        _white: "#FFFFFF",
+        _red: "#EA5555",
+        light_Red: "#FF9898"
+      }
     },
   },
   plugins: [],
