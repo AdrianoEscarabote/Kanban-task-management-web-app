c33631ba8b12ad07967c40f4f2d38601162e19e6
AdrianoEscarabote <drian.escarabote@hotmail.com>
2023-04-18 13:08:57 -0300

    custom

--- src/custom/boardNames/useBoardNames.ts
+++ src/custom/boardNames/useBoardNames.ts
@@ -1,24 +1,25 @@
-import { useEffect, useState } from "react";
-import { BoardDataType } from "./boardNames";
+import { useEffect } from "react";
 import axios from "axios";
+import { useDispatch } from "react-redux";
+import { setBoards } from "@/redux/board/reducer";
 
 const useBoardNames = () => {
-  const [boardNames, setBoardNames] = useState<BoardDataType>({ boards: [] });
+  const dispatch = useDispatch()
 
   useEffect(() => {
     const fetchData = async () => {
       try {
         const response = await axios.get("data/data.json");
-        setBoardNames(response.data);
+        // Chama a ação setBoards para atualizar o estado no Redux com os dados recebidos
+        dispatch(setBoards(response.data))
       } catch (err) {
         console.log(err);
       }
     }
+    console.log("entrei no use effect")
 
     fetchData()
   }, [])
-
-  return { boardNames }
 }
 
 export default useBoardNames;
