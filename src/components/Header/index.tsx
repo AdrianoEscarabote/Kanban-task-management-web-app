b3e5a555fc60837968701482a8b3619cbbb9759b
AdrianoEscarabote <drian.escarabote@hotmail.com>
2023-05-02 12:52:04 -0300

    bug fix

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -7,26 +7,40 @@ import Sidebar from "../SideBar";
 import Ellipsis from "../shared/EllipsisBoard";
 import AddTaskModal from "../AddTaskModal";
 import style from "./style.module.css"
+import SidebarMobile from "../SidebarMobile";
  
 const Header: React.FC<HeaderProps> = ({ open }) => {
   const [modalOpen, setModalOpen] = useState<boolean>(false)
   const { theme } = useSelector((rootReducer: rootState) => rootReducer.themeReducer)
   const { nameBoard } = useSelector((rootReducer: rootState) => rootReducer.reducerNameBoard)
   const boardNames = useSelector((rootReducer: rootState) => rootReducer.boardSlice)
+  
+  const [sidebarOpen, setSidebarOpen] = useState<boolean>(false)
 
   const handleClickOpenModal = () => {
     setModalOpen(!modalOpen)
   }
 
+  const handleOpenSidebar = () => {
+    setSidebarOpen(!sidebarOpen)
+  }
+
   return (
     <header className={`${style.header} w-full z-20 border-b fixed flex ${theme === "light" ? "bg-_white border-light_Blue" : "bg-dark_Gray border-medium_Gray"}`}>
       <nav aria-label="main" className="w-full flex items-center justify-between">
+
         <Sidebar />
+
+        {
+          sidebarOpen ? <SidebarMobile handleModal={handleOpenSidebar} /> : null
+        }
+
         <picture className="my-5 mx-5 h-auto">
           <source media="(max-width:620px)" srcSet={"/assets/logo-mobile.svg"} width="24" height="25" />
           <Image 
             style={{ width: "auto" }}  
-            src={theme === "light" ? "/assets/logo-dark.svg" : "/assets/logo-light.svg"} height="25" 
+            src={theme === "light" ? "/assets/logo-dark.svg" : "/assets/logo-light.svg"} 
+            height="25" 
             width="160" 
             alt="Kanban - Home" 
             priority={true} 
@@ -36,6 +50,23 @@ const Header: React.FC<HeaderProps> = ({ open }) => {
         <div className={`${style.parent_items} py-5 relative px-5 border-l content flex items-center justify-between w-full ${theme === "light" ? "border-light_Blue" : "border-medium_Gray"}`}>
 
           <h2 className={`${style.h2} text-2xl font-bold ${theme === "light" ? "text-black" : "text-white"}`}>{!nameBoard ? boardNames.boards[0]?.name : nameBoard }</h2>
+
+          <button 
+            onClick={handleOpenSidebar}
+            className={`${style.button_sidebar} text-2xl font-bold items-center gap-2
+            ${theme === "light" 
+            ? "text-black" 
+            : "text-white"}`
+            } >
+              {!nameBoard ? boardNames.boards[0]?.name : nameBoard }
+              <Image 
+                className="mt-1"
+                src={sidebarOpen ? "/assets/icon-chevron-up.svg" : "/assets/icon-chevron-down.svg"} 
+                width={8}
+                height={5.59}
+                alt=""
+              />
+            </button>
 
           <div className="wrapper_buttons flex items-center justify-center">
             {<button onClick={handleClickOpenModal} className={`${style.buttonDesktop} bg-purple_Dark text-_white w-40 h-12 rounded-3xl mr-5 text-sm font-bold hover:bg-purple_Light`}>+ Add new Task</button>}
