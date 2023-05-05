72629754442b4405c9a7c61d19bd62f8ecd8b5c3
AdrianoEscarabote <drian.escarabote@hotmail.com>
2023-05-05 11:04:11 -0300

    mobile menu

---
+++ src/components/MobileMenu/MobileMenuProps.d.ts
@@ -0,0 +1,5 @@
+interface MobileMenuProps {
+  handleModal: () => void
+}
+
+export { MobileMenuProps }
---
+++ src/components/MobileMenu/index.tsx
@@ -0,0 +1,28 @@
+import { useSelector } from "react-redux"
+import { rootState } from "@/redux/reduxTypes"
+import InputToggle from "../shared/InputToggle"
+import React from "react"
+import ListBoardNames from "../shared/ListBoardNames"
+import style from "./style.module.css"
+import { MobileMenuProps } from "./MobileMenuProps"
+
+const MobileMenu: React.FC<MobileMenuProps> = ({ handleModal }) => {
+  const { theme } = useSelector((rootReducer: rootState) => rootReducer.themeReducer)
+
+  const handleModalOpen = () => {
+    handleModal()
+  }
+  
+  return (
+    <div className={`${style.parent} parent_modal`} onClick={() => handleModalOpen()}>
+      <div onClick={(e) => e.stopPropagation()} className={`${style.sidebar} fixed left-0 top-0 min-h-full w-80 flex flex-col ${theme === "light" ? "bg-_white border-r border-light_Blue" : "bg-dark_Gray border-r border-medium_Gray"}`} >
+        <ListBoardNames />
+        <div className={`${style.parent_buttons} w-full absolute bottom-5 flex flex-col gap-6 items-center justify-center`}>
+          <InputToggle />
+        </div>
+      </div>  
+    </div> 
+  )
+};
+
+export default MobileMenu;
