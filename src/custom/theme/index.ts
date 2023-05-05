74ffdc3673795a75300538d77caf977301917c4b
AdrianoEscarabote <drian.escarabote@hotmail.com>
2023-05-05 11:02:16 -0300

    custom hook - theme

---
+++ src/custom/theme/index.ts
@@ -0,0 +1,18 @@
+import { rootState } from "@/redux/reduxTypes";
+import { setLocalStorageValue } from "@/redux/theme/actions";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+
+export const useTheme = () => {
+  const dispatch = useDispatch();
+  const { theme } = useSelector((state: rootState) => state.themeReducer);
+
+  useEffect(() => {
+    const localStorageTheme = localStorage.getItem("theme");
+    if (localStorageTheme && localStorageTheme !== theme) {
+      dispatch(setLocalStorageValue(localStorageTheme));
+    }
+  }, [dispatch, theme]);
+
+  return theme;
+};
