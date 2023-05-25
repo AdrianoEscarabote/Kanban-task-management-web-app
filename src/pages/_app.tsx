840019a31f1cdab056b6898148bf48791216de4a
AdrianoEscarabote <drian.escarabote@hotmail.com>
2023-05-25 16:56:42 -0300

    pages

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,13 @@
-import Layout from "@/components/Layout"
 import "../styles/global.css"
 import type { AppProps } from 'next/app'
 import { Provider } from 'react-redux'
 import store from '@/redux/store'
 
 export default function App({ Component, pageProps }: AppProps) {
+  
   return (
     <Provider store={store}>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </Provider>
+      <Component {...pageProps} />
+    </Provider>        
   ) 
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,25 @@
 import Head from "next/head";
 import Board from "@/components/Board";
+import Layout from "@/components/Layout";
+import useBoardData from "@/custom/boardData/useBoardData";
+import Preloader from "@/components/preloader";
 
 const Home = () => {
+  const { loading } = useBoardData()
 
   return (
     <>
       <Head>
         <title>Frontend Mentor | Kanban task management web app</title>
-      </Head>
-      <Board />
+      </Head>    
+      {
+        loading 
+        ? <Preloader />
+        :  
+        <Layout>
+          <Board />
+        </Layout>
+      }   
     </>
   );
 }
