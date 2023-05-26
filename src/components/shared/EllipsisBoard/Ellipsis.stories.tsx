b95e2ab6206a2c63dfcd915939fd6f5c2b54e509
AdrianoEscarabote <drian.escarabote@hotmail.com>
2023-05-26 11:45:47 -0300

    storybook

--- src/components/shared/EllipsisBoard/Ellipsis.stories.tsx
+++ src/components/shared/EllipsisBoard/Ellipsis.stories.tsx
@@ -4,7 +4,7 @@ import { Provider } from "react-redux";
 import store from "../../../redux/store";
 
 export default {
-  title: "shared/Ellipsis",
+  title: "shared/Ellipsis Board",
   component: Ellipsis,
   parameters: {
     layout: 'fullscreen',
