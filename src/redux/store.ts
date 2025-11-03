946f87b5972826f35f7e703b68238f69e08369e1
AdrianoEscarabote <drian.escarabote@hotmail.com>
2025-11-03 17:05:08 -0300

    refactor(redux): update middleware configuration in store setup

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,10 +1,10 @@
-import { configureStore } from "@reduxjs/toolkit"
-import logger from "redux-logger"
-import rootReducer from "./root-reducer"
+import { configureStore } from "@reduxjs/toolkit";
+import logger from "redux-logger";
+import rootReducer from "./root-reducer";
 
 const store = configureStore({
   reducer: rootReducer,
-  middleware: [logger]
-})
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
+});
 
-export default store
+export default store;
